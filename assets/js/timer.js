9488a4c5390d170f451daac773d51240d0675845
stacy <stacykillinger@gmail.com>
2018-07-07 17:38:04 -0400

    initial js commit after redo

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -7,12 +7,16 @@ let duration;
 let diffTime;
 let timeCount;
 let time;
+let myAudio;
+
+//start is hidden until a time is chosen
 startBtn.disabled = true;
 startBtn.style.visibility = 'hidden';
 
 function update(t) {
+  box.classList.toggle("hide");
   time = t;
-  timer.innerHTML = t + ":00";
+  timer.innerHTML = t + ":00";  //display before starting
   btn3.disabled = true;
   btn5.disabled = true;
   btn7.disabled = true;
@@ -28,14 +32,30 @@ function meditateNow() {
   duration = moment.duration(duration.asMilliseconds() - 1000, 'milliseconds');
   let min = moment.duration(duration).minutes();
   let sec = moment.duration(duration).seconds();
-  timer.innerHTML = min + ":" + sec;
+  //if less than 9 sec left pad with a zero in front
+  if(sec <= 9){
+    timer.innerHTML = min + ":0" + sec;
+  }else{
+    timer.innerHTML = min + ":" + sec;
+  }
+  //when timer is done
   if(sec <= 0 && min <= 0) {
     clearInterval(timeCount);
-    timer.innerHTML = "You have completed " + time + " minutes of meditation";
+    //timer.innerHTML = "You have completed " + time + " minutes of meditation";
   }
 }
 
 function startInterval() {
+  if(time == 3){
+    myAudio = new Audio ("working_tools/track1.mp3");
+  }
+else if(time == 5) {
+    myAudio = new Audio ("working_tools/track2.mp3");
+  }
+  else if(time == 7) {
+    myAudio = new Audio ("working_tools/track3.mp3");
+  }
+  myAudio.play();
   if(diffTime > 0) {
     timeCount = setInterval(meditateNow, 1000);
   }
